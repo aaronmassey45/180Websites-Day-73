942e5861d918bb7ced6f1518e9221bd8ff65600a
aaronmassey45 <aaronmassey45@gmail.com>
2017-10-09 15:46:35 -0500

    refactor/ Switched to bootstrap 4 navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
-import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import React, {Component} from 'react';
 
 export default class NewNavbar extends Component {
   render() {
     return (
-      <Navbar inverse>
-        <Navbar.Header>
-          <Navbar.Brand>
-            {this.props.brand}
-          </Navbar.Brand>
-          <Navbar.Toggle/>
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav pullRight>
-            <NavItem href="http://aaronmassey.pro">Home</NavItem>
-            <NavItem href="https://medium.com/@aaronmassey45">Blog</NavItem>
-            <NavItem href="https://github.com/aaronmassey45">Github</NavItem>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+        <span className="navbar-brand">{this.props.brand}</span>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <ul className="navbar-nav ml-auto">
+            <li><a className="nav-item nav-link" href="http://aaronmassey.pro">Home</a></li>
+            <li><a className="nav-item nav-link" href="https://medium.com/@aaronmassey45">Blog</a></li>
+            <li><a className="nav-item nav-link" href="https://github.com/aaronmassey45">Github</a></li>
+          </ul>
+        </div>
+      </nav>
+    )
   }
 }
