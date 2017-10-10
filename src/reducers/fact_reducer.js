81e9d3e7cba71954e12c4b222f1c502d19c49d13
aaronmassey45 <aaronmassey45@gmail.com>
2017-10-09 22:33:53 -0500

    refactor/ Added in error message

--- src/reducers/fact_reducer.js
+++ src/reducers/fact_reducer.js
@@ -1,4 +1,4 @@
-import { SUCCESS } from '../actions';
+import { SUCCESS, FAILURE } from '../actions';
 
 const INITIAL_STATE = {
   fact: ''
@@ -8,6 +8,8 @@ export default function date_fact(state=INITIAL_STATE, action) {
   switch (action.type) {
     case SUCCESS:
       return { ...state, fact: action.payload }
+    case FAILURE:
+      return { ...state, fact: 'You entered a incorrect date. try a date like 8/8' }
     default:
       return state;
   }
